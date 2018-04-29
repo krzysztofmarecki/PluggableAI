151a7e14da4705e062600b3e128dbd754e7cbfd5
krzysztofmarecki <pawianekjunior@gmail.com>
2018-04-29 15:06:22 +0200

    fixed logic for determining ability to attack,
    added distance to Raycast
    refactor

--- Scripts/AttackAction.cs
+++ Scripts/AttackAction.cs
@@ -14,40 +14,30 @@ public class AttackAction : Action {
     {
         Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * controller.enemyStats.attackRange, Color.red);
         
-        var target = controller.target;
-
-        
-       
-        if (inConeFight(target, controller))
+        Transform target = controller.target;
+        if (isTargetInRange(target, controller))
         {
             // Call attack method
+            // remember to delete controller.enemyStats.attackRange, attackRange should be weapon's variable
 
         }
     }
 
-    bool inConeFight(Transform target, StateController controller)
+    bool isTargetInRange(Transform target, StateController controller)
     {
         float distance = Vector3.Distance(target.position, controller.eyes.transform.position);
-        Debug.Log("distance" + distance);
 
         // check distance
         if (distance <= controller.enemyStats.attackRange)
         {
-            // calculate and check angle
             Vector3 direction = (target.position - controller.eyes.transform.position);
-            var angle = Vector3.Angle(controller.eyes.transform.forward, direction);
-            Debug.Log("angle: " + angle);
-            if (angle <= controller.enemyStats.coneSightAngle)
+
+            // check if is actually in sight (no X-Ray allowed)
+            RaycastHit hit;
+            if (Physics.Raycast(controller.eyes.transform.position, direction, out hit, distance))
             {
-                // check if is actually in sight (no X-Ray allowed)
-                RaycastHit hit;
-                if (Physics.Raycast(controller.eyes.transform.position, direction, out hit))
-                {
-                    Debug.DrawRay(controller.eyes.transform.position, direction, Color.red);
-                    //    Debug.Log(hit.transform.ToString());
-                    //    Debug.Log(target.ToString());
-                    return hit.transform == target;
-                }
+                Debug.DrawRay(controller.eyes.transform.position, direction, Color.red);
+                return hit.transform == target;
             }
         }
         return false;
