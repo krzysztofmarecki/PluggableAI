0d6bcaee4e79323452f3faa3249e40edaea427e1
krzysztofmarecki <pawianekjunior@gmail.com>
2018-04-29 15:17:45 +0200

    removed unused variables

--- Scripts/EnemyStats.cs
+++ Scripts/EnemyStats.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "PluggableAI/EnemyStats")]
 public class EnemyStats : ScriptableObject {
     
-	public float moveSpeed = 1;                 // not used so far
 	public float lookRange = 40f;               // LookDecision
-	public float lookSphereCastRadius = 1f;     // not used so far (for debug only)
     public float coneSightAngle = 65f;          // LookDecision, AttacAction
 
     public float attackRange = 1f;              // AttackAction in future as weaponStat
